6eef66a483d54f45d298e5214ce7900474967a7c
whk06061 <gju06061@naver.com>
2022-12-11 14:34:25 +0900

    refactor: 코드 다듬기

--- src/main/java/racingcar/constants/ErrorMessage.kt
+++ src/main/java/racingcar/constants/ErrorMessage.kt
@@ -5,7 +5,7 @@ enum class ErrorMessage(private val message: String) {
     NAME_LENGTH("${PREFIX.message} 이름은 5자 이하만 가능합니다."),
     NOT_NUMBER("${PREFIX.message} 숫자만 입력해주세요.");
 
-    fun getMessage():String{
+    fun getMessage(): String {
         return this.message
     }
 }
