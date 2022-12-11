6425ac9c439f9846c613898b7e39b946ca7d4e7a
whk06061 <gju06061@naver.com>
2022-12-11 14:17:31 +0900

    feat: 무작위 값을 구한 후 전진하는 기능 구현

--- src/main/java/racingcar/domain/Car.kt
+++ src/main/java/racingcar/domain/Car.kt
@@ -1,6 +1,21 @@
 package racingcar.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+import racingcar.constants.Numbers
+
 class Car(private val name: String) {
-    private val position = 0
+    private var position = 0
     // 추가 기능 구현
+
+    fun move() {
+        val randomNumber = Randoms.pickNumberInRange(
+            Numbers.MINIMUM_RANDOM_NUMBER_RANGE.getNumber(),
+            Numbers.MAXIMUM_RANDOM_NUMBER_RANGE.getNumber()
+        )
+        if (randomNumber >= Numbers.MOVE_NUMBER_STANDARD.getNumber()) position += 1
+    }
+
+    fun getPosition():Int{
+        return position
+    }
 }
